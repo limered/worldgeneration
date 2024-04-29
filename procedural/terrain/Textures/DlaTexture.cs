7e3cc52c0a51c6653ee2efa05885ff2c904f8d1e
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-04-29 16:32:30 +0200

    draw texture pixel from tree

--- procedural/terrain/Textures/DlaTexture.cs
+++ procedural/terrain/Textures/DlaTexture.cs
@@ -1,15 +1,38 @@
-﻿using Godot;
+﻿using System.Collections.Generic;
+using Godot;
 
 namespace dla_terrain.procedural.terrain.Textures;
 
 public class DlaTexture
 {
+    private readonly RandomNumberGenerator _rnd;
+    private readonly List<Vector2I> _tree = new();
+
+    private Image _image;
+    
+    public DlaTexture()
+    {
+        _rnd = new RandomNumberGenerator();
+        _rnd.Seed = (ulong)GD.Hash("emil");
+    }
+    
     public ImageTexture Create()
     {
-        var image = Image.Create(8, 8, false, Image.Format.Rgbaf);
-        image.Fill(Colors.Black);
-        image.SetPixel(4, 4, Colors.White);
+        _tree.Add(new Vector2I(3, 3));
         
-        return ImageTexture.CreateFromImage(image);
+        _image = Image.Create(8, 8, false, Image.Format.Rgbaf);
+        _image.Fill(Colors.Black);
+        DrawTree();
+        
+        return ImageTexture.CreateFromImage(_image);
+    }
+
+    private void DrawTree()
+    {
+        for (var i = 0; i < _tree.Count; i++)
+        {
+            var point = _tree[i];
+            _image.SetPixel(point.X, point.Y, Colors.White);
+        }
     }
 }
