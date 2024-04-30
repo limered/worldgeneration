317c82a1f06568f8d16ce58f330bb9eb6ca18c6f
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-04-30 12:05:38 +0200

    only connect to same generation of points

--- procedural/terrain/Textures/DlaAlgorithm.cs
+++ procedural/terrain/Textures/DlaAlgorithm.cs
@@ -5,6 +5,12 @@ using Godot;
 
 namespace dla_terrain.Procedural.Terrain.Textures;
 
+public struct DlaPoint
+{
+    public Vector2I Position { get; set; }
+    public int GenerationLayer { get; set; }
+}
+
 public class DlaAlgorithm
 {
     private const int StartSize = 8;
@@ -13,27 +19,28 @@ public class DlaAlgorithm
 
     private readonly Image[] _layers = new Image[LayerCount];
     private readonly RandomNumberGenerator _rnd;
-    private List<Vector2I> _tree = new();
+    private List<DlaPoint> _tree = new();
 
-    private Vector2I _center;
-    private Vector2I _walker;
+    private DlaPoint _center;
+    private DlaPoint _walker;
 
     public DlaAlgorithm(string seed)
     {
         _rnd = new RandomNumberGenerator();
-        if(!string.IsNullOrEmpty(seed))
-        {
-            _rnd.Seed = (ulong)GD.Hash(seed);
-        }
+        if (!string.IsNullOrEmpty(seed)) _rnd.Seed = (ulong)GD.Hash(seed);
     }
 
     public ImageTexture Create()
     {
         InitLayers();
-        
-        _center = new Vector2I(StartSize / 2, StartSize/2);
+
+        _center = new DlaPoint
+        {
+            Position = new Vector2I(StartSize / 2, StartSize / 2),
+            GenerationLayer = 0
+        };
         _tree.Add(_center);
-        
+
         AddNewPixelsToTree(0);
 
         int i;
@@ -41,55 +48,35 @@ public class DlaAlgorithm
         {
             ExpandTree();
             FillGaps();
-            // RotateTree();
             AddNewPixelsToTree(i);
         }
-        
-        DrawTree(i-1);
 
-        return ImageTexture.CreateFromImage(_layers[i-1]);
-    }
+        DrawTree(i - 1);
 
-    private void RotateTree()
-    {
-        const float rotationParam = Mathf.Pi / 18;
-        var theta = _rnd.RandfRange(-rotationParam, rotationParam);
-        var transform = new Transform2D(theta, Vector2.Zero);
-        var tempTree = new Vector2I[_tree.Count];
-        for (var i = 0; i < _tree.Count; i++)
-        {
-            var pixel = _tree[i];
-            pixel -= _center;
-            pixel = (Vector2I)(pixel * transform);
-            pixel += _center;
-
-            tempTree[i] = pixel;
-        }
-
-        _tree = tempTree.ToList();
+        return ImageTexture.CreateFromImage(_layers[i - 1]);
     }
 
     private void FillGaps()
     {
-        var tempTree = new List<Vector2I>();
+        var tempTree = new List<DlaPoint>();
         for (var i = 0; i < _tree.Count; i++)
         {
             var point = _tree[i];
-            if(i == 0) continue;
-            var j = i-1;
-            while (j >= 0 && _tree[j].X <= point.X && _tree[j].X >= point.X - 2)
+            if (i == 0) continue;
+            var j = i - 1;
+            while (j >= 0 && _tree[j].Position.X <= point.Position.X && _tree[j].Position.X >= point.Position.X - 2)
             {
                 // Check for top connection
-                if (_tree[j].X == point.X && _tree[j].Y == point.Y - 2)
-                {
-                    tempTree.Add(new Vector2I(point.X, point.Y-1));
-                }
+                if (_tree[j].Position.X == point.Position.X &&
+                    _tree[j].Position.Y == point.Position.Y - 2 &&
+                    _tree[j].GenerationLayer == point.GenerationLayer)
+                    tempTree.Add(point with { Position = new Vector2I(point.Position.X, point.Position.Y - 1) });
+                
                 // Check for left connection
-                if (_tree[j].X == point.X - 2 && _tree[j].Y == point.Y)
-                {
-                    tempTree.Add(new Vector2I(point.X-1, point.Y));
-                    
-                }
+                if (_tree[j].Position.X == point.Position.X - 2 &&
+                    _tree[j].Position.Y == point.Position.Y &&
+                    _tree[j].GenerationLayer == point.GenerationLayer)
+                    tempTree.Add(point with { Position = new Vector2I(point.Position.X - 1, point.Position.Y) });
 
                 j--;
             }
@@ -100,22 +87,22 @@ public class DlaAlgorithm
 
     private void ExpandTree()
     {
-        var tempTree = new Vector2I[_tree.Count];
+        var tempTree = new DlaPoint[_tree.Count];
         for (var i = 0; i < _tree.Count; i++)
         {
-            var pixel = _tree[i];
+            var pixelPosition = _tree[i].Position;
             // translate
-            pixel -= _center;
+            pixelPosition -= _center.Position;
             // scale
-            pixel *= 2;
+            pixelPosition *= 2;
             // translateBack
-            pixel += _center * 2;
+            pixelPosition += _center.Position * 2;
 
-            tempTree[i] = pixel;
+            tempTree[i].Position = pixelPosition;
         }
 
-        _center *= 2;
-        _tree = tempTree.OrderBy(v => v.X).ThenBy(v => v.Y).ToList();
+        _center.Position *= 2;
+        _tree = tempTree.OrderBy(v => v.Position.X).ThenBy(v => v.Position.Y).ToList();
     }
 
     private void InitLayers()
@@ -131,7 +118,7 @@ public class DlaAlgorithm
     private void AddNewPixelsToTree(int layerId)
     {
         var border = layerId == 0 ? StartSize : StartSize * (2 << (layerId - 1));
-        
+
         for (var walkerIndex = 0; walkerIndex < NewPixelsPerLayer * (layerId + 1); walkerIndex++)
         {
             SpawnNewPoint(layerId);
@@ -140,14 +127,15 @@ public class DlaAlgorithm
             while (!stuck)
             {
                 var vel = Velocity();
-                var dir = ((Vector2)_center - _walker).Normalized() * (float)0.3;
-                _walker += vel + (Vector2I)dir;
+                var dir = ((Vector2)_center.Position - _walker.Position).Normalized() * (float)0.3;
+                _walker.Position += vel + (Vector2I)dir;
 
-                _walker.X = Math.Clamp(_walker.X, 0, border - 1);
-                _walker.Y = Math.Clamp(_walker.Y, 0, border - 1);
+                var nextPosition = new Vector2I(Math.Clamp(_walker.Position.X, 0, border - 1),
+                    Math.Clamp(_walker.Position.Y, 0, border - 1));
+                _walker.Position = nextPosition;
 
                 stuck = _tree
-                    .Select(point => (point - _walker).Length())
+                    .Select(point => (point.Position - _walker.Position).Length())
                     .Any(dist => dist <= 1.0);
 
                 if (!stuck) continue;
@@ -163,10 +151,12 @@ public class DlaAlgorithm
         var direction = _rnd.RandiRange(0, 4);
         _walker = direction switch
         {
-            0 => new Vector2I(0, _rnd.RandiRange(0, border - 1)),
-            1 => new Vector2I(_rnd.RandiRange(0, border - 1), 0),
-            2 => new Vector2I(border - 1, _rnd.RandiRange(0, border - 1)),
-            _ => new Vector2I(_rnd.RandiRange(0, border - 1), border - 1)
+            0 => new DlaPoint { Position = new Vector2I(0, _rnd.RandiRange(0, border - 1)), GenerationLayer = layerId },
+            1 => new DlaPoint { Position = new Vector2I(_rnd.RandiRange(0, border - 1), 0), GenerationLayer = layerId },
+            2 => new DlaPoint
+                { Position = new Vector2I(border - 1, _rnd.RandiRange(0, border - 1)), GenerationLayer = layerId },
+            _ => new DlaPoint
+                { Position = new Vector2I(_rnd.RandiRange(0, border - 1), border - 1), GenerationLayer = layerId }
         };
     }
 
@@ -182,7 +172,7 @@ public class DlaAlgorithm
         for (var i = 0; i < _tree.Count; i++)
         {
             var point = _tree[i];
-            _layers[layerId].SetPixel(point.X, point.Y, Colors.White);
+            _layers[layerId].SetPixel(point.Position.X, point.Position.Y, Colors.White);
         }
     }
 }
