63ad40afe3d8e3cee3baa154ab4e7e65ce93cc18
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-05-09 00:41:40 +0200

    use distance to uniformly distribute points

--- procedural/terrain/Poisson/DiscSamplingAlgorithm.cs
+++ procedural/terrain/Poisson/DiscSamplingAlgorithm.cs
@@ -9,7 +9,7 @@ public class DiscSamplingAlgorithm
 {
     private const int Resolution = 1024;
     private const int HalfResolution = Resolution / 2;
-    private const int R = 40;
+    private const int R = 20;
     private const int R2 = R * 2;
     private const int K = 30;
     private readonly Vector2 _empty = new(-1, -1);
@@ -28,7 +28,7 @@ public class DiscSamplingAlgorithm
     {
         _rnd = new RandomNumberGenerator();
         if (!string.IsNullOrEmpty(seed)) _rnd.Seed = (ulong)GD.Hash(seed);
-        
+
         _cellCount = Resolution / _cellSize;
         _grid = new Vector2[_cellCount * _cellCount];
     }
@@ -47,53 +47,81 @@ public class DiscSamplingAlgorithm
 
     private int Vector2Index(int x, int y)
     {
+        x = Math.Clamp(x, 0, _cellCount - 1);
+        y = Math.Clamp(y, 0, _cellCount - 1);
         return x + y * _cellCount;
+    }
+
+    private Vector2I ToCell(Vector2 v)
+    {
+        return new Vector2I((int)(v.X / _cellSize), (int)(v.Y / _cellSize));
     }
 
     private int ToCellIndex(Vector2 v)
     {
-        var x = v.X / _cellSize;
-        var y = v.Y / _cellSize;
-        return Vector2Index((int)x, (int)y);
+        var vI = ToCell(v);
+        return Vector2Index(vI.X, vI.Y);
+    }
+
+    private Vector2[] Neighbours(Vector2I v)
+    {
+        return new[]
+        {
+            _grid[Vector2Index(v.X - 1, v.Y - 1)],
+            _grid[Vector2Index(v.X - 1, v.Y)],
+            _grid[Vector2Index(v.X - 1, v.Y + 1)],
+            _grid[Vector2Index(v.X, v.Y - 1)],
+            _grid[Vector2Index(v.X, v.Y + 1)],
+            _grid[Vector2Index(v.X + 1, v.Y - 1)],
+            _grid[Vector2Index(v.X + 1, v.Y)],
+            _grid[Vector2Index(v.X + 1, v.Y + 1)]
+        };
     }
 
     public ImageTexture Create()
     {
         IsGenerating = true;
         Init();
-        
+
         while (_activeList.Any())
         {
             var i = _rnd.RandiRange(0, _activeList.Count - 1);
             var xI = _grid[_activeList[i]];
-            var pointAdded = false;
+            var found = false;
             for (var k = 0; k < K; k++)
             {
-                var rad = _rnd.RandfRange(0, Mathf.Tau);
-                var dir = new Vector2(Mathf.Cos(rad), Mathf.Sin(rad)).Normalized();
+                var theta = _rnd.RandfRange(0, Mathf.Tau);
+                var dir = new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
                 dir *= _rnd.RandfRange(R, R2);
-                var newPoint = dir + xI;
-                var cellIndex = ToCellIndex(newPoint);
-                if (cellIndex >= _grid.Length ||
-                    cellIndex < 0 ||
-                    _grid[cellIndex] != _empty) continue;
+                var sample = dir + xI;
+                var sampleCellIndex = ToCellIndex(sample);
+                if (sampleCellIndex >= _grid.Length ||
+                    sampleCellIndex < 0 ||
+                    _grid[sampleCellIndex] != _empty) continue;
+                
+                var neighbours = Neighbours(ToCell(sample));
+                var ok = true;
+                var n = 0;
+                while (ok && n < 8)
+                {
+                    ok &= (sample - neighbours[n++]).Length() >= R;
+                }
 
-                pointAdded = true;
-                _grid[cellIndex] = newPoint;
-                _activeList.Add(cellIndex);
+                if (!ok) continue;
+                found = true;
+                _grid[sampleCellIndex] = sample;
+                _activeList.Add(sampleCellIndex);
             }
 
-            // ToDo: Check if all neighbours are set
-            if (!pointAdded) _activeList.RemoveAt(i);
+            if (!found) _activeList.RemoveAt(i);
         }
 
-        for (var p = 0; p < _grid.Length; p++)
+        foreach (var point in _grid)
         {
-            var point = _grid[p];
             if (point != _empty &&
                 point.X is < Resolution and >= 0 &&
                 point.Y is < Resolution and >= 0)
-                _image.SetPixel((int)point.X, (int)point.Y, Color.Color8(255, 0, 0));
+                _image.SetPixel((int)point.X, (int)point.Y, Color.Color8(255, 255, 255));
         }
 
         IsGenerating = false;
