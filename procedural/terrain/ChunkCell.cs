bddcccbbe32c1278f28595392a26062d0044f762
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-05-11 00:34:54 +0200

    generate first chunk

---
+++ procedural/terrain/ChunkCell.cs
@@ -0,0 +1,35 @@
+﻿using dla_terrain.Utils.Random;
+using Godot;
+
+namespace dla_terrain.Procedural.Terrain;
+
+public class ChunkCell
+{
+    private readonly int _cellSize;
+    public Vector2I Coordinate { get; private set; }
+    public Vector3 CenterPoint { get; private set; }
+    public bool IsRendered { get; set; }
+    private readonly int _masterSeed;
+
+    private readonly RandomNumberGenerator _rnd = new();
+
+    public ChunkCell(string masterSeed, int cellSize)
+    {
+        _cellSize = cellSize;
+        _masterSeed = GD.Hash(masterSeed);
+    }
+
+    public void ReGenerate(Vector2I coordinate)
+    {
+        Coordinate = coordinate;
+        _rnd.Seed = SmallXxHash.Seed(_masterSeed)
+            .Eat(Coordinate.X).Eat(Coordinate.Y);
+
+        CenterPoint = new Vector3(
+            _rnd.RandfRange(-_cellSize / 2f, _cellSize / 2f),
+            0,
+            _rnd.RandfRange(-_cellSize / 2f, _cellSize / 2f));
+        
+        GD.Print(CenterPoint);
+    }
+}
---
+++ procedural/terrain/Map.cs
@@ -0,0 +1,46 @@
+﻿using Godot;
+
+namespace dla_terrain.Procedural.Terrain;
+
+public partial class Map : Node
+{
+    [Export] private string _masterSeed;
+    [Export] private int _initialChunkCount;
+    [Export] private int _maxChunkCount;
+
+    [Export] private int _chunkRadius;
+    
+    private ChunkCell[] _chunks;
+
+    private const int K = 30;
+    private int _r2;
+    private int _cellSize;
+
+
+    public override void _Ready()
+    {
+        _cellSize = (int)(_chunkRadius / Mathf.Sqrt(2));
+        
+        _chunks = new ChunkCell[_maxChunkCount];
+        _chunks[0] = new ChunkCell(_masterSeed, _cellSize);
+        _chunks[0].ReGenerate(new Vector2I(0, 0));
+    }
+
+    public override void _Process(double delta)
+    {
+        RenderChunks();
+    }
+
+    private void RenderChunks()
+    {
+        foreach (var chunk in _chunks)
+        {
+            if(chunk is null or { IsRendered: true }) continue;
+            chunk.IsRendered = true;
+            var chunkScene = GD.Load<PackedScene>("res://Scenes/chunk_center.tscn");
+            var sceneInstance = chunkScene.Instantiate<Node3D>();
+            AddChild(sceneInstance);
+            sceneInstance.Translate(chunk.CenterPoint);
+        }
+    }
+}
