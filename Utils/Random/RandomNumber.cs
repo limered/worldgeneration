e6585303e81b377ad16d5dcc59a75f09e86ac901
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-05-10 23:41:07 +0200

    generate custom rng numbers

---
+++ Utils/Random/RandomNumber.cs
@@ -0,0 +1,69 @@
+﻿using Godot;
+
+namespace dla_terrain.Utils.Random;
+
+public class RandomNumber
+{
+    private readonly SmallXxHash _hash;
+    private long _counter;
+
+    public RandomNumber(int? seed)
+    {
+        _hash = SmallXxHash.Seed(seed ?? GD.Hash(GD.Randi()));
+    }
+
+    public void Reset()
+    {
+        _counter = 0;
+    }
+
+    public uint LastUint()
+    {
+        return _hash;
+    }
+
+    public int LastInt()
+    {
+        return (int)(uint)_hash;
+    }
+
+    public float LastFloat()
+    {
+        var v = (uint)_hash;
+        var f = (int)v & 255;
+        return f * (1f / 255);
+    }
+    
+    public uint Uint()
+    {
+        return _hash.Eat((byte)_counter++);
+    }
+
+    public uint Range(uint start, uint stop)
+    {
+        return Uint() % (stop - start) + start;
+    }
+
+    public int Int()
+    {
+        return (int)(uint)_hash.Eat((byte)_counter++);
+    }
+
+    public int Range(int start, int end)
+    {
+        var v = _hash.Eat((byte)_counter++);
+        return (int)(v % (end - start) + start);
+    }
+
+    public float Float()
+    {
+        var v = (uint)_hash.Eat((byte)_counter++);
+        var f = (int)v & 255;
+        return f * (1f / 255);
+    }
+
+    public float Range(float start, float end)
+    {
+        return Float() * (end - start) + start;
+    }
+}
---
+++ Utils/Random/SmallXxHash.cs
@@ -0,0 +1,53 @@
+﻿namespace dla_terrain.Utils.Random;
+
+public readonly struct SmallXxHash
+{
+    private const uint PrimeA = 0b10011110001101110111100110110001;
+    private const uint PrimeB = 0b10000101111010111100101001110111;
+    private const uint PrimeC = 0b11000010101100101010111000111101;
+    private const uint PrimeD = 0b00100111110101001110101100101111;
+    private const uint PrimeE = 0b00010110010101100110011110110001;
+
+    private readonly uint _accumulator;
+
+    public static implicit operator uint(SmallXxHash hash)
+    {
+        var avalanche = hash._accumulator;
+        avalanche ^= avalanche >> 15;
+        avalanche *= PrimeB;
+        avalanche ^= avalanche >> 13;
+        avalanche *= PrimeC;
+        avalanche ^= avalanche >> 16;
+        return avalanche;
+    }
+
+    public static implicit operator SmallXxHash(uint accumulator)
+    {
+        return new SmallXxHash(accumulator);
+    }
+
+    private SmallXxHash(uint accumulator)
+    {
+        _accumulator = accumulator;
+    }
+
+    public static SmallXxHash Seed(int seed)
+    {
+        return (uint)seed + PrimeE;
+    }
+
+    public SmallXxHash Eat(int data)
+    {
+        return RotateLeft(_accumulator + (uint)data * PrimeC, 17) * PrimeD;
+    }
+
+    public SmallXxHash Eat(byte data)
+    {
+        return RotateLeft(_accumulator + data * PrimeE, 11) * PrimeA;
+    }
+
+    private static uint RotateLeft(uint data, int steps)
+    {
+        return (data << steps) | (data >> (32 - steps));
+    }
+}
