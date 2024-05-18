abf0a3c1aa807e53bcd1a0b674d81b0882d45160
Emil Bohleber <me@limered.de>
2024-05-19 01:38:14 +0200

    add cache for landmarks and XxHash extensions to get Float easy

---
+++ Utils/Random/SmallXxHashExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace dla_terrain.Utils.Random;
+
+public static class SmallXxHashExtensions
+{
+    private const float FloatFactor = 1f / 255;
+
+    public static byte GetByte(this SmallXxHash hash, int i)
+    {
+        return i switch
+        {
+            0 => (byte)(hash & 255),
+            1 => (byte)((hash >> 8) & 255),
+            2 => (byte)((hash >> 16) & 255),
+            3 => (byte)((hash >> 24) & 255),
+            _ => (byte)(hash & 255)
+        };
+    }
+
+    public static float Float01A(this SmallXxHash hash) => FloatFactor * (hash & 255);
+    public static float Float01B(this SmallXxHash hash) => FloatFactor * ((hash >> 8) & 255);
+    public static float Float01C(this SmallXxHash hash) => FloatFactor * ((hash >> 16) & 255);
+    public static float Float01D(this SmallXxHash hash) => FloatFactor * ((hash >> 24) & 255);
+}
---
+++ procedural/terrain/LandmarkCache.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace dla_terrain.Procedural.Terrain;
+
+public class LandmarkCache
+{
+    private readonly Dictionary<Vector2I, Landmark> _cache = new();
+
+    public Landmark Retrieve(Vector2I coord)
+    {
+        return _cache[coord];
+    }
+
+    public void Cache(Vector2I coord, Landmark landmark)
+    {
+        _cache[coord] = landmark;
+    }
+
+    public bool Contains(Vector2I coord)
+    {
+        return _cache.ContainsKey(coord);
+    }
+}
