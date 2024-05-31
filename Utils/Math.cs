90f0e316df22846afd4bfd025400c4cdcdb126ab
Emil Bohleber <emil.bohleber@brickmakers.de>
2024-05-31 12:49:56 +0200

    add better lerp

---
+++ Utils/Math.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace dla_terrain.Utils;
+
+public static class Math
+{
+    public static float ExpDecay(float a, float b, float decay = 16f, float dt = 1f)
+    {
+        return b + (a - b) * MathF.Exp(-decay * dt);
+    }
+}
